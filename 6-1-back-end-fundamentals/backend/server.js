5e7fe5bfe498b5bfab64aeddee08b251712eb491
Dalia Alquwaidhi <s202282360@kfupm.edu.sa>
2026-03-30 11:52:49 +0300

    Complete TODO 5 home route

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -5,9 +5,9 @@ const app = express();
 
 app.use(cors());
 
-// TODO 5: Create the home route "/"
-
-
+app.get('/', (req, res) => {
+  res.send('Hello from the back-end server');
+});
 
 // TODO 6: Create the "/about" route
 
