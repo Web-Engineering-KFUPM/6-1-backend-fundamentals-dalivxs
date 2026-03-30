382276e41484f5762c9dcb2b44b24a58343f4c7e
Dalia Alquwaidhi <s202282360@kfupm.edu.sa>
2026-03-30 11:54:55 +0300

    Complete TODO 6 about route

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -9,9 +9,9 @@ app.get('/', (req, res) => {
   res.send('Hello from the back-end server');
 });
 
-// TODO 6: Create the "/about" route
-
-
+app.get('/about', (req, res) => {
+  res.send('This is the about route');
+});
 
 // TODO 7: Create the "/student" route
 
